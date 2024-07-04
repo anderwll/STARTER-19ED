95481e99836cf658111e3f931ba06c1f1c556240
Johann Patricio Daniel <156346733+JohannPDaniel@users.noreply.github.com>
2024-07-03 23:37:54 -0300

    fazendo botao de ver senha

--- BLOCO-1/FRONT-3/Aula19_03.07_App-Recado-Profile/js/index.js
+++ BLOCO-1/FRONT-3/Aula19_03.07_App-Recado-Profile/js/index.js
@@ -3,64 +3,76 @@ const modalSignup = new bootstrap.Modal('#modal-signup')
 const formLogin = document.getElementById('form-login')
 const formSignup = document.getElementById('form-signup')
 
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#password');
+const icon = togglePassword.querySelector('i');
+
+togglePassword.addEventListener('click', function () {
+  const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+  password.setAttribute('type', type);
+
+  icon.classList.toggle('bi-eye');
+  icon.classList.toggle('bi-eye-slash');
+});
+
 formLogin.addEventListener('submit', async (event) => {
   event.preventDefault()
-//checkvalidity verifica se o elemento está correto, verificando o required, minLength etc - retorna true ou false
-    if (!formLogin.checkValidity()) {
-        event.stopPropagation()
-        formLogin.classList.add('was-validated')
-        return
-      }
+  //checkvalidity verifica se o elemento está correto, verificando o required, minLength etc - retorna true ou false
+  if (!formLogin.checkValidity()) {
+    event.stopPropagation()
+    formLogin.classList.add('was-validated')
+    return
+  }
 
-      const email = event.target.email.value
-      const password = event.target.password.value
+  const email = event.target.email.value
+  const password = event.target.password.value
 
-      showLoading(true, "btn-login")
-      const response = await postLogin(email, password)
-      showLoading(false, "btn-login")
+  showLoading(true, "btn-login")
+  const response = await postLogin(email, password)
+  showLoading(false, "btn-login")
 
-      if(!response.success) {
-        alertSign(true, "danger", response.message, "feedback-login")
-        return
-      }
-      
-      alertSign(true, "success", response.message, "feedback-login")      
-      formLogin.reset()      
-      document.location.href = './home.html'
+  if (!response.success) {
+    alertSign(true, "danger", response.message, "feedback-login")
+    return
+  }
+
+  alertSign(true, "success", response.message, "feedback-login")
+  formLogin.reset()
+  document.location.href = './home.html'
 })
 
 formSignup.addEventListener('submit', async (event) => {
   event.preventDefault()
 
-    if (!formSignup.checkValidity()) {
-        event.stopPropagation()
-        formSignup.classList.add('was-validated')
-        return
-      }
+  if (!formSignup.checkValidity()) {
+    event.stopPropagation()
+    formSignup.classList.add('was-validated')
+    return
+  }
 
-      const name = event.target['name-signup'].value
-      const email = event.target['email-signup'].value
-      const password = event.target['password-signup'].value
-      const passwordConfirm = event.target['password-confirm'].value
+  const name = event.target['name-signup'].value
+  const email = event.target['email-signup'].value
+  const password = event.target['password-signup'].value
+  const passwordConfirm = event.target['password-confirm'].value
 
-      if(password !== passwordConfirm) {
-        alertSign(true, "danger", "Senhas não conferem!", "feedback-signup")
-        return
-      }
+  if (password !== passwordConfirm) {
+    alertSign(true, "danger", "Senhas não conferem!", "feedback-signup")
+    return
+  }
 
-      showLoading(true, "btn-confirm-signup")
+  showLoading(true, "btn-confirm-signup")
 
-      const response = await postSignup(name, email, password)
+  const response = await postSignup(name, email, password)
 
-      showLoading(false, "btn-confirm-signup")
+  showLoading(false, "btn-confirm-signup")
 
-      if(!response.success) {
-        return alertToast(response.message, "danger")
-      }      
-      alertToast(response.message, "success")      
-      
-      formSignup.reset()     
-      setTimeout(() => {
-        modalSignup.hide()
-      }, 2000)
+  if (!response.success) {
+    return alertToast(response.message, "danger")
+  }
+  alertToast(response.message, "success")
+
+  formSignup.reset()
+  setTimeout(() => {
+    modalSignup.hide()
+  }, 2000)
 })
