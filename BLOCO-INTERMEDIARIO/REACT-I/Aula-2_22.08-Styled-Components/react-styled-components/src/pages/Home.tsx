caa59ce251668443272d68d3463b70b3f5d4f5a3
anderwll <ander.wilmsen98@gmail.com>
2024-08-22 22:45:46 -0300

    aula 22.08 - styled components

--- BLOCO-INTERMEDIARIO/REACT-I/Aula-2_22.08-Styled-Components/react-styled-components/src/pages/Home.tsx
+++ BLOCO-INTERMEDIARIO/REACT-I/Aula-2_22.08-Styled-Components/react-styled-components/src/pages/Home.tsx
@@ -1,22 +1,9 @@
-import { Button } from "../components/styleds/Button";
-import { MainBanner } from "../components/styleds/MainBanner";
-import { Title } from "../components/styleds/Title";
+import { Banner } from "../components/Banner";
 
 export function Home() {
   return (
     <>
-      <MainBanner>
-        <Title color="white">Meu titulo</Title>
-
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-          velit, veritatis.
-        </p>
-
-        <Button>Saiba mais</Button>
-      </MainBanner>
-
-      <Title>Outro titulo</Title>
+      <Banner />
     </>
   );
 }
