045b5e47d7284c5d6dbb5eab12deb6cd58f8fd32
anderwll <ander.wilmsen98@gmail.com>
2024-09-03 21:11:20 -0300

    contador

--- BLOCO-INTERMEDIARIO/REACT-I/Aula-5_03.09-Hooks-useState/react-useState/src/components/styleds/Container.tsx
+++ BLOCO-INTERMEDIARIO/REACT-I/Aula-5_03.09-Hooks-useState/react-useState/src/components/styleds/Container.tsx
@@ -1,26 +1,30 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 interface ContainerProps {
-  flexDirection?: 'row' | 'column';
+  flexDirection?: "row" | "column";
   notPadding?: boolean;
   gap?: string;
 }
 
 export const Container = styled.section<ContainerProps>`
   display: flex;
-  flex-direction: ${(props) => props.flexDirection ?? 'row'};
-  justify-content: space-around;
+  flex-direction: ${(props) => props.flexDirection ?? "row"};
+  justify-content: center;
   align-items: center;
   text-align: center;
-  gap: ${(props) => props.gap ?? 0};
+  gap: ${(props) => props.gap ?? "14px"};
   width: 100%;
   height: auto;
   margin: auto;
-  padding: ${(props) => (props.notPadding ? '0' : '70px 0')};
+  padding: ${(props) => (props.notPadding ? "0" : "70px 0")};
 
   max-width: 1444px;
 
-  @media screen and (max-width: 768px) {
+  h1 {
+    font-size: 48px;
+  }
+
+  @media screen and (max-width: 480px) {
     flex-direction: column;
   }
 `;
--- BLOCO-INTERMEDIARIO/REACT-I/Aula-5_03.09-Hooks-useState/react-useState/src/pages/Home.tsx
+++ BLOCO-INTERMEDIARIO/REACT-I/Aula-5_03.09-Hooks-useState/react-useState/src/pages/Home.tsx
@@ -1,9 +1,72 @@
+import { useState } from "react";
+import { Button } from "../components/styleds/Button";
+import { Container } from "../components/styleds/Container";
 import { DefaultLayout } from "../config/layouts/DefaultLayout";
 
+/**
+ * Events
+ *
+ * Quando não necessita de paramentro
+ * onClick={minhaFuncao}
+ *
+ * Quando necessitar
+ * onClick={() => minhaFuncao(valo1)}
+ */
+
+/**
+ * O useState retorna um array com 2 posições
+ * 1ª posição: O estado atual
+ * 2ª posição: Função para atualizar o estado
+ *
+ * SINTAXE - useState
+ * const [meuEstado, setMeuEstado] = useState(valorInial)
+ */
+
 export function Home() {
+  const [contador, setContador] = useState<number>(0);
+
+  function incrementar() {
+    setContador(contador + 1);
+    console.log("Incrementando....", contador);
+  }
+
+  function decrementar() {
+    setContador((prevState) => {
+      console.log("PREV -->", prevState);
+
+      if (prevState === 0) {
+        return 0;
+      }
+
+      return prevState - 1;
+    });
+
+    console.log("Decrementando....");
+  }
+
   return (
     <DefaultLayout>
-      <h1>Home</h1>
+      <Container>
+        <Button onClick={() => decrementar()}>-</Button>
+
+        <h1>Contador: {contador}</h1>
+
+        <Button onClick={incrementar}>+</Button>
+      </Container>
     </DefaultLayout>
   );
 }
+
+// CONTADOR SEM useState
+
+// let contador: number = 0;
+
+// function incrementar() {
+//   contador++;
+//   console.log("Incrementando....", contador);
+// }
+
+// function decrementar() {
+//   contador--;
+//   console.log("Decrementando....", contador);
+// }
