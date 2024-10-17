7ea444b91ba00d6231eaca580baebe5ddc362b51
anderwll <ander.wilmsen98@gmail.com>
2024-10-17 19:52:55 -0300

    config express

--- BLOCO-INTERMEDIARIO/BANCO-DADOS-II/academy-api/src/server.ts
+++ BLOCO-INTERMEDIARIO/BANCO-DADOS-II/academy-api/src/server.ts
@@ -1,1 +1,20 @@
-console.log("Funcionou");
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    ok: true,
+    message: "Api Academy",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}!`);
+});
