416485bbb0f62ef2a97c965c9c1c8ae9037016dd
Johann Patricio Daniel <156346733+JohannPDaniel@users.noreply.github.com>
2024-11-11 19:30:39 -0300

    fazendo função de delete

--- BLOCO-INTERMEDIARIO/REACT-II/web-academy/src/configs/services/assessment.service.ts
+++ BLOCO-INTERMEDIARIO/REACT-II/web-academy/src/configs/services/assessment.service.ts
@@ -1,84 +1,103 @@
 import {
-  Assessment,
-  CreateAssessmentRequest,
-  UpdateAssessmentRequest,
-} from "../../types/assessment.type";
-import { api, ResponseApi } from "./api.service";
+	Assessment,
+	CreateAssessmentRequest,
+	UpdateAssessmentRequest,
+} from '../../types/assessment.type';
+import { api, ResponseApi } from './api.service';
 
 export async function getAssessments(token: string) {
-  try {
-    // http://localhost:3000
-    const response = await api.get<ResponseApi<Assessment[]>>("/assessments", {
-      headers: {
-        Authorization: token,
-      },
-    });
+	try {
+		// http://localhost:3000
+		const response = await api.get<ResponseApi<Assessment[]>>('/assessments', {
+			headers: {
+				Authorization: token,
+			},
+		});
 
-    return {
-      ok: response.data.ok,
-      message: response.data.message,
-      data: response.data.data,
-    };
-  } catch (error: any) {
-    return {
-      ok: error.response.data.ok,
-      message: `Erro: ${error.response.data.message}`,
-    };
-  }
+		return {
+			ok: response.data.ok,
+			message: response.data.message,
+			data: response.data.data,
+		};
+	} catch (error: any) {
+		return {
+			ok: error.response.data.ok,
+			message: `Erro: ${error.response.data.message}`,
+		};
+	}
 }
 
 export async function createAssessment(
-  token: string,
-  dataBody: CreateAssessmentRequest
+	token: string,
+	dataBody: CreateAssessmentRequest
 ) {
-  try {
-    const response = await api.post<ResponseApi<Assessment>>(
-      "/assessments",
-      dataBody,
-      {
-        headers: {
-          Authorization: token,
-        },
-      }
-    );
+	try {
+		const response = await api.post<ResponseApi<Assessment>>(
+			'/assessments',
+			dataBody,
+			{
+				headers: {
+					Authorization: token,
+				},
+			}
+		);
 
-    return {
-      ok: response.data.ok,
-      message: response.data.message,
-      data: response.data.data,
-    };
-  } catch (error: any) {
-    return {
-      ok: error.response.data.ok,
-      message: `Erro: ${error.response.data.message}`,
-    };
-  }
+		return {
+			ok: response.data.ok,
+			message: response.data.message,
+			data: response.data.data,
+		};
+	} catch (error: any) {
+		return {
+			ok: error.response.data.ok,
+			message: `Erro: ${error.response.data.message}`,
+		};
+	}
 }
 
 export async function updateAssessment(
-  token: string,
-  { id, ...dataBody }: UpdateAssessmentRequest
+	token: string,
+	{ id, ...dataBody }: UpdateAssessmentRequest
 ) {
-  try {
-    const response = await api.put<ResponseApi<Assessment>>(
-      `/assessments/${id}`,
-      dataBody,
-      {
-        headers: {
-          Authorization: token,
-        },
-      }
-    );
+	try {
+		const response = await api.put<ResponseApi<Assessment>>(
+			`/assessments/${id}`,
+			dataBody,
+			{
+				headers: {
+					Authorization: token,
+				},
+			}
+		);
+
+		return {
+			ok: response.data.ok,
+			message: response.data.message,
+			data: response.data.data,
+		};
+	} catch (error: any) {
+		return {
+			ok: error.response.data.ok,
+			message: `Erro: ${error.response.data.message}`,
+		};
+	}
+}
+
+export async function deleteAssessment(token: string, id: string) {
+	try {
+		const response = await api.delete<ResponseApi<Assessment>>(`/assessments/${id}`, {
+			headers: { Authorization: token },
+		});
 
     return {
       ok: response.data.ok,
       message: response.data.message,
-      data: response.data.data,
-    };
-  } catch (error: any) {
-    return {
-      ok: error.response.data.ok,
-      message: `Erro: ${error.response.data.message}`,
-    };
-  }
+      data: response.data.data
+    }
+	} catch (error: any) {
+		return {
+			ok: error.response.data.ok,
+			message: `Erro: ${error.response.data.message}`,
+		};
+	}
 }
