0e58928f676adf119fc2aa22d8145552a1dcac04
anderwll <ander.wilmsen98@gmail.com>
2024-11-26 23:25:38 -0300

    section chips

---
+++ BLOCO-AVANCADO/REACT-III/react-mui/src/components/SectionChips/index.tsx
@@ -0,0 +1,56 @@
+import { Search, ShareRounded } from "@mui/icons-material";
+import {
+  Box,
+  Chip,
+  Grid2,
+  IconButton,
+  InputAdornment,
+  TextField,
+  useMediaQuery,
+} from "@mui/material";
+
+const categories = ["All categories", "Company", "Design", "Engineering"];
+
+export function SectionChips() {
+  const mobile = useMediaQuery("(max-width:600px)");
+
+  return (
+    <Grid2
+      size={12}
+      display="flex"
+      flexDirection={{ xs: "column", md: "row" }}
+      justifyContent="space-between"
+      alignItems="center"
+      gap={2}
+    >
+      <Box display="flex" gap={{ xs: 0, md: 2 }}>
+        {categories.map((cat) => (
+          <Chip
+            label={cat}
+            onClick={() => alert(cat)}
+            size={mobile ? "small" : "medium"}
+          />
+        ))}
+      </Box>
+
+      <Box>
+        <TextField
+          placeholder="Search..."
+          size="small"
+          slotProps={{
+            input: {
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Search />
+                </InputAdornment>
+              ),
+            },
+          }}
+        />
+        <IconButton>
+          <ShareRounded />
+        </IconButton>
+      </Box>
+    </Grid2>
+  );
+}
--- BLOCO-AVANCADO/REACT-III/react-mui/src/pages/Blog.tsx
+++ BLOCO-AVANCADO/REACT-III/react-mui/src/pages/Blog.tsx
@@ -1,3 +1,19 @@
+import { Grid2, Typography } from "@mui/material";
+import { SectionChips } from "../components/SectionChips";
+
 export function Blog() {
-  return <h1>Blog</h1>;
+  return (
+    <Grid2 container spacing={2}>
+      <Grid2 size={12}>
+        <Typography variant="h2">Blog</Typography>
+      </Grid2>
+      <Grid2 size={12}>
+        <Typography variant="body1">
+          Stay in the loop with the latest about our products
+        </Typography>
+      </Grid2>
+
+      <SectionChips />
+    </Grid2>
+  );
 }
