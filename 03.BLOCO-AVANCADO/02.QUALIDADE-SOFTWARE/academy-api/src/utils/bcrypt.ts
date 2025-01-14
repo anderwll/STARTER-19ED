9cba35be4c2af4597511b0619611c2440727c619
Johann Patricio Daniel <156346733+JohannPDaniel@users.noreply.github.com>
2025-01-14 20:32:38 -0300

    documentando o metodo verify

--- 03.BLOCO-AVANCADO/02.QUALIDADE-SOFTWARE/academy-api/src/utils/bcrypt.ts
+++ 03.BLOCO-AVANCADO/02.QUALIDADE-SOFTWARE/academy-api/src/utils/bcrypt.ts
@@ -1,28 +1,38 @@
 import bcrypt from "bcrypt";
 
 /**
- * @name Bcrypt
+ * @class Bcrypt
  * @description É uma adaptação da biblioteca bcrypt.
  * @see Documentação {@link https://www.npmjs.com/package/bcrypt}
  * @author Anderson Wilmsen
  */
 export class Bcrypt {
-  /**
-   * @name generateHash()
-   * @description Método responsável por gerar um hash.
-   * @param password Senha que será encriptografada.
-   * @returns Hash - Hash gerado.
-   * @example
-   * const bcrypt = new Bcrypt();
-   * const passwordHash = await bcrypt.generateHash(password);
-   */
-  public async generateHash(password: string): Promise<string> {
-    const hash = await bcrypt.hash(password, Number(process.env.BCRYPT_SALT));
-    return hash;
-  }
+	/**
+	 * @name generateHash()
+	 * @description Método responsável por gerar um hash.
+	 * @param password Senha que será encriptografada.
+	 * @returns Hash - Hash gerado.
+	 * @example
+	 * const bcrypt = new Bcrypt();
+	 * const passwordHash = await bcrypt.generateHash(password);
+	 */
+	public async generateHash(password: string): Promise<string> {
+		const hash = await bcrypt.hash(password, Number(process.env.BCRYPT_SALT));
+		return hash;
+	}
 
-  public async verify(password: string, hash: string): Promise<boolean> {
-    const isValid = await bcrypt.compare(password, hash);
-    return isValid;
-  }
+	/**
+   *  @name verify()
+   *  @description Método para verificar se a senha fornecida e o hash são equivalentes.
+   *  @param password Senha no formato "string" fornecido para comparação. 
+   *  @param hash - String encriptografada para comparação.
+   *  @returns Retorna uma Promise de boolean.
+   *  @example
+   *  const bcrypt = new Bcrypt();
+   *  const isValid = await bcrypt.verify(password, hash);
+   */
+	public async verify(password: string, hash: string): Promise<boolean> {
+		const isValid = await bcrypt.compare(password, hash);
+		return isValid;
+	}
 }
