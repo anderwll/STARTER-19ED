823de4b088d4d8800e31c1fcb76c9fa2a69edebb
anderwll <ander.wilmsen98@gmail.com>
2025-01-21 19:54:19 -0300

    config jest

---
+++ 03.BLOCO-AVANCADO/02.QUALIDADE-SOFTWARE/academy-api/jest.config.ts
@@ -0,0 +1,17 @@
+export default {
+  // Configura o Jest para testes no Node.js
+  // usando Typescript.
+  preset: "ts-jest",
+  testEnvironment: "node",
+  transform: {
+    ".+\\.ts$": "ts-jest",
+  },
+
+  // Informa o diretório onde os testes estarão contidos.
+  roots: ["<rootDir>/tests"],
+
+  // Configurações de cobertura de código.
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+  coverageDirectory: "coverage",
+  coveragePathIgnorePatterns: ["\\\\node_modules\\\\"],
+};
